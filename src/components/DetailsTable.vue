6950b8c7c054432e0a66d29db27492ebcd7ee0dc
Shafik <shaifk108@gmail.com>
2024-04-15 16:44:16 +0600

    Excel Download

--- src/components/DetailsTable.vue
+++ src/components/DetailsTable.vue
@@ -8,9 +8,35 @@ function selectVal(event) {
   //   console.log(inputVal)
   navigator.clipboard.writeText(text)
 }
+
+function downloadData() {
+  const dataStr = generateExcelData(userData)
+  const blob = new Blob([dataStr], { type: 'application/vnd.ms-excel' })
+  const url = window.URL.createObjectURL(blob)
+  const link = document.createElement('a')
+  link.href = url
+  link.setAttribute('download', 'data.xls')
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+}
+
+function generateExcelData(data) {
+  let result = 'Product Name\tColor\tCategory\tPrice\tAction\tAction\n'
+  data.forEach((item) => {
+    result += `${item.url}\t${item.element_1}\t${item.element_2}\t${item.element_4}\t${item.element_5}\t${item.element_6}\n`
+  })
+  return result
+}
 </script>
 <template>
   <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <button
+      @click="downloadData"
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-4"
+    >
+      Download
+    </button>
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
       <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <tr>
